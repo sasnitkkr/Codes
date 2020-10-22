fa4f56f35473ccb9a2174120a17ba9420e79ab9f
ss <72294816+sas-28@users.noreply.github.com>
2020-10-22 15:41:08 +0530

    Return Nth fibonacci number

--- DP/NthFibonacciNo.cpp
+++ DP/NthFibonacciNo.cpp
@@ -1,1 +1,25 @@
-
+//F0=0, F1=1, F2=1, F3=2,......
+//CALCULATE nth fibonacci number
+#include<bits/stdc++.h>
+using namespace std;
+int memo[1000];
+int fib(int n)
+{
+    if(n==0 || n==1)
+        return n;
+    if(memo[n]==-1)
+    {
+        int res;
+        res=fib(n-1)+fib(n-2);
+        memo[n]=res;
+    }
+    return memo[n];
+}
+int main()
+{
+    int n;
+    cin>>n;
+    memset(memo,-1, sizeof(memo));
+    cout<<fib(n);
+    return 0;
+}
