1bec9955d0a7cb09d180c37a1d0fbe119bf76018
sas <72294816+sasnitkkr@users.noreply.github.com>
2021-05-03 21:44:25 +0530

    Implementation using 'triplet struct'

---
+++ Number Theory/extendedEuledianAlgo-1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+              
+struct triplet{
+  int gcd;
+  int x;
+  int y;
+};               
+                                                         
+triplet gcdExtended(int a, int b) // returns gcd, updates x, y
+{                  
+    // Base case
+    
+    if(a==0)
+    {
+        triplet ans;
+        ans.x = 0;
+        ans.y = 1;
+        ans.gcd = b;
+        return ans;
+    }
+    
+    // Extended Euclid Algorithm 
+    
+    triplet tmp = gcdExtended(b%a, a);
+    triplet ans;
+    
+    int x1 = tmp.x;
+    int y1 = tmp.y;
+    
+    ans.x = y1 - (b/a)*x1;
+    ans.y = x1;
+    ans.gcd = tmp.gcd;
+    
+    return ans;
+}                                                           
+                                                           
+                                                           
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    triplet ans = gcdExtended(a,b);
+    cout<<ans.gcd<<" "<<ans.x<<" "<<ans.y<<"\n";
+                                                         
+    return 0;
+}
