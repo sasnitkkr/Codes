db875c56a52babb586ea3e1ba0fd99a47a5598c5
Sahil Shende <72294816+sasnitkkr@users.noreply.github.com>
2021-05-30 13:14:15 +0530

    Create subsets_of_array.cpp

---
+++ Arrays/subsets_of_array.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void subset(vector<int>& v, int n, vector<vector<int>>& output)
+{
+    if(n==1)
+    {
+        vector<int> v1;
+        vector<int> v2(1, v[n-1]);
+        output.push_back(v1);
+        output.push_back(v2);
+        return;
+    }
+    subset(v, n-1, output);
+    int cn=output.size();
+    for(int i=cn; i<2*cn; i++)
+    {   
+        vector<int> tmp(output[i%cn]);
+        tmp.push_back(v[n-1]);
+        output.push_back(tmp);
+    }
+}
+
+void run_case()                                                            
+{                                                           
+    vector<vector<int>> output;
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++)
+        cin>>v[i];
+    subset(v, n, output);
+    for(int i=0; i<output.size(); i++)
+    {
+        for(int j=0; j<output[i].size(); j++)
+            cout<<output[i][j]<<" ";
+        cout<<"\n";
+    }
+}                                       
+                                                           
+                                                           
+int main()
+{
+   
+                                                           
+    int t=1;                                               
+    //cin>>t;                                                
+    while(t--)                                             
+    {                                                      
+        run_case();                                           
+    }                                                      
+    return 0;
+}
