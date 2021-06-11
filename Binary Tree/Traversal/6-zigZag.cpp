743e9a695d7426137c69c4b78b78f82270ad6794
Sahil Shende <72294816+sasnitkkr@users.noreply.github.com>
2021-06-11 22:39:59 +0530

    Create 6-zigZag.cpp

---
+++ Binary Tree/Traversal/6-zigZag.cpp
@@ -0,0 +1,34 @@
+vector <int> zigZagTraversal(Node* root)
+{
+	// Code here
+	vector<int> op;
+    if(root==NULL)
+        return op;
+	stack<Node*> s1, s2;
+	s1.push(root);
+	while(!s1.empty()||!s2.empty())
+	{
+	    Node* curr;
+	    while(!s1.empty())
+	    {
+	        curr = s1.top();
+	        s1.pop();
+	        op.push_back(curr->data);
+	        if(curr->left!=NULL)
+	            s2.push(curr->left);
+	        if(curr->right!=NULL)
+	            s2.push(curr->right);
+	    }
+	    while(!s2.empty())
+	    {
+	        curr = s2.top();
+	        s2.pop();
+	        op.push_back(curr->data);
+	        if(curr->right!=NULL)
+	            s1.push(curr->right);
+	        if(curr->left!=NULL)
+	            s1.push(curr->left);
+	    }
+	}
+	return op;
+}
